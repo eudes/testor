7f33c43ca43cb5ece57b38494107ece263f16ec0
eudes <eudes8@gmail.com>
2017-06-14 17:40:24 +0200

    asdf

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ restService.post('/hook', function (req, res) {
 
                 if (requestBody.result.action) {
                     speech += 'action: ' + requestBody.result.action;
+                    console.log(requestBody.result.action);
                 }
             }
         }

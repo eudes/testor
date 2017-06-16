968400bf920ede37fe956760ddf704519beef279
eudes <eudes8@gmail.com>
2017-06-16 21:44:15 +0200

    subida parcial de automatización de tests mediante requests

--- server/test/actions.js
+++ server/test/actions.js
@@ -1,13 +1,17 @@
-var assert = require('chai').assert;
+const assert = require('chai').assert;
 const fs = require('fs');
+const path = require('path');
 
 const actions = require('../actions.js');
 
-
+const sampleRequestsFolder = ('sampleRequests');
 
 describe('actions', function(){
 	describe('sum()', function(){
-		let tests = 
+		let tests = findFilesInPath(sampleRequestsFolder, /^sum_.*.json$/)
+			.map((filename) => {
+				request: fs.readFileSync(filename),
+				response: null) // TODO: generar respuestas automáticamente?
 
 		tests.forEach(function (test) {
 			it('correctly adds ' + test.args.length + ' args', function() {
@@ -24,3 +28,23 @@ describe('actions', function(){
 	});
 
 });
+
+function findFilesInPath(startPath, filterRE) {
+
+    var result = [];
+
+    if (!fs.existsSync(startPath)) {
+        throw "No directory " + startPath;
+    }
+
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        var stat = fs.lstatSync(filename);
+        if (filter.test(filename)) {
+            result.push(filename);
+        };
+    };
+
+    return result;
+}
